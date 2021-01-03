0165affd19d89d97f4d35edde15cc6031f7c2514
qiuhuachuan <qiuhuachuan@yeah.net>
2021-01-03 21:59:39 +0800

    两数相加

---
+++ ch2/add_two_numbers.py
@@ -0,0 +1,26 @@
+'''
+两数相加
+'''
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    '''
+    两数相加
+    '''
+    def add_two_numbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        # 创建一个节点值为 None 的头结点，result 和 p 指向头结点，result 用来最后返回，p 用来遍历
+        result = p = ListNode(None)
+        s = 0  # 初始化进位 s 为 0
+        while l1 or l2 or s:
+            # 如果 l1 或 l2 存在, 则取l1的值 + l2的值 + s(s初始为0, 如果下面有进位1, 下次加上)
+            s += (l1.val if l1 else 0) + (l2.val if l2 else 0)
+            p.next = ListNode(s % 10)  # p.next 指向新链表, 用来创建一个新的链表
+            p = p.next  # p 向后遍历
+            s = s // 10  # 有进位情况则取模, eg. s = 18, 18 // 10 = 1
+            l1 = l1.next if l1 else None  # 如果l1存在, 则向后遍历, 否则为 None
+            l2 = l2.next if l2 else None  # 如果l2存在, 则向后遍历, 否则为 None
+        # 返回 result 的下一个节点, 因为 result 指向的是空的头结点, 下一个节点才是新建链表的后序节点
+        return result.next
